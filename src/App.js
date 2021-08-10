9253f0b0e80dd446ea5c19f5a71e55f7fd2a5880
Felipe Schmidt <schmidtfe2@gmail.com>
2021-08-10 11:23:46 -0300

    Adição do sistema de tema e adaptação na aplicação

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Cabecalho from "./Components/Cabecalho";
 import Container from "./Components/Container";
+import SwitcherTema from "./Components/SwitcherTema";
 import { GlobalStyle } from "./Components/GlobalStyle";
+import { ThemeProvider } from "styled-components";
+import { BtnTema } from './Components/UI'
+import { temaClaro, temaEscuro } from './Components/UI/temas'
 
 function App() {
+
+  const [tema, toggleTema] = useState(true);
+
+  function handleClick(){
+    toggleTema(!tema);
+  }
+
   return (
-    <>
-      <GlobalStyle/>
+    <ThemeProvider theme={tema ? temaClaro : temaEscuro}>
+      <GlobalStyle />
+      <BtnTema onClick={handleClick}>
+        <SwitcherTema tema={tema}/>
+      </BtnTema>
       <Cabecalho />
       <Container />
-    </>
+    </ThemeProvider>
   );
 }
 
